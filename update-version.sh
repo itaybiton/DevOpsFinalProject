b1d63187d83d3806093ec42d7b920c35d4767aff
Amit Asher <asheramit4@gmail.com>
2023-01-09 22:56:37 +0200

    use generic path on deploy script

--- update-version.sh
+++ update-version.sh
@@ -1,4 +1,4 @@
-WORKDIR="C:\Users\Amit Asher\Desktop"
+WORKDIR="C:/"
 TMPDIR="DevOpsFinalProject"
 GITREPO="https://github.com/itaybiton/DevOpsFinalProject.git"
 RESOURCE="C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps\omer-dan-itay-amit"
@@ -11,14 +11,14 @@ echo " | (_| |  __/ |_) | | (_) | |_| | | |_) | | | (_) | (_| |"
 echo "  \__,_|\___| .__/|_|\___/ \__, | | .__/|_|  \___/ \__,_|"
 echo "            | |             __/ | | |                    "
 echo "            |_|            |___/  |_|                    "
-
+echo ""
 
 cd "${WORKDIR}"
 # DELETE FOLDER IF EXISTS
-echo "delete tmp folder if exists: ${TMPDIR}"
+echo "delete tmp folder if exists: ${WORKDIR}/${TMPDIR}"
 rm -rf "${TMPDIR}"
 # CLONE GIT REPO
-echo "cloning project... ${GITREPO}"
+echo "cloning project: ${GITREPO} to tmp folder: ${WORKDIR}/${TMPDIR}"
 git clone "${GITREPO}"
 cd "${TMPDIR}"
 echo "" # new line
