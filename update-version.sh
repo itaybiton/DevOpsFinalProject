3ea7cbd029c520118d46d87e3b43225af54c0b0d
Amit Asher <asheramit4@gmail.com>
2023-01-09 22:21:08 +0200

    fix deploy script

--- update-version.sh
+++ update-version.sh
@@ -3,18 +3,34 @@ TMPDIR="DevOpsFinalProject"
 GITREPO="https://github.com/itaybiton/DevOpsFinalProject.git"
 RESOURCE="C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps\omer-dan-itay-amit"
 
-cd $WORKDIR
+echo "      _            _                                   _ "
+echo "     | |          | |                                 | |"
+echo "   __| | ___ _ __ | | ___  _   _   _ __  _ __ ___   __| |"
+echo "  / _  |/ _ \   _ | |/ _ \| | | | |  _ \|  __/ _ \ / _  |"
+echo " | (_| |  __/ |_) | | (_) | |_| | | |_) | | | (_) | (_| |"
+echo "  \__,_|\___| .__/|_|\___/ \__, | | .__/|_|  \___/ \__,_|"
+echo "            | |             __/ | | |                    "
+echo "            |_|            |___/  |_|                    "
+
+
+cd "${WORKDIR}"
 # DELETE FOLDER IF EXISTS
-rm -rf $TMPDIR
+echo "delete tmp folder if exists: ${TMPDIR}"
+rm -rf "${TMPDIR}"
 # CLONE GIT REPO
-git clone $GITREPO
-cd $TMPDIR
+echo "cloning project... ${GITREPO}"
+git clone "${GITREPO}"
+cd "${TMPDIR}"
+echo "" # new line
 
 # REMOVE PREVIOUS RESOURCE
+echo "remove previous version from: ${RESOURCE}"
 rm -rf "${RESOURCE}"
 # CREATE NEW RESOURCE FOLDER
 mkdir "${RESOURCE}"
 # COPY RESOURCE TO TARGET FOLDER
+echo "copy new version (index.jsp) to: ${RESOURCE}"
 cp index.jsp "${RESOURCE}"
 
+echo "cleaning tmp folder: ${WORKDIR}/${TMPDIR}"
 rm -rf "${WORKDIR}/${TMPDIR}"
